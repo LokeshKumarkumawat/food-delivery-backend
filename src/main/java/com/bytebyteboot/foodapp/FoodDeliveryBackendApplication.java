f0795eb1dc282d733dc21dc31717e41bb65c659b
lokesh <lokeshkumargithub@gmail.com>
2025-11-29 12:15:24 +0530

    feat(email): implement async email notification service

--- src/main/java/com/bytebyteboot/foodapp/FoodDeliveryBackendApplication.java
+++ src/main/java/com/bytebyteboot/foodapp/FoodDeliveryBackendApplication.java
@@ -3,9 +3,11 @@ package com.bytebyteboot.foodapp;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @Slf4j
 @SpringBootApplication
+@EnableAsync
 public class FoodDeliveryBackendApplication {
 
 	public static void main(String[] args) {
