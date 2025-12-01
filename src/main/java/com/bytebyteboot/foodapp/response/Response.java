8a5c7b58eeb6d8b4dec6d481f989daca3302e231
lokesh <lokeshkumargithub@gmail.com>
2025-12-01 18:59:16 +0530

    feat(cache): implement Redis caching across all service layers with proper cacheable and eviction rules

--- src/main/java/com/bytebyteboot/foodapp/response/Response.java
+++ src/main/java/com/bytebyteboot/foodapp/response/Response.java
@@ -1,13 +1,17 @@
 package com.bytebyteboot.foodapp.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.Map;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Response<T> {
